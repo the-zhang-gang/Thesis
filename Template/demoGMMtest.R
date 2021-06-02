8ac680d03947dbe595622d9ae10fb4727c47d7e4
enjofaes <enjo.faes@hotmail.com>
2021-06-02 11:06:24 +0200

    pushed update ESTest signifc. plots, ACD equation

--- Template/demoGMMtest.R
+++ Template/demoGMMtest.R
@@ -24,12 +24,17 @@ z = rugarch::residuals(fit[[n.list]],standardize=T)
 tmp.5<- rugarch::GMMTest(z)
 tmp.5
 
+par(mfrow=c(5,1), mar = c(1,1,1,1))
+plot(fit[[1]],which=11)
+plot(fit[[2]],which=11)
+plot(fit[[3]],which=11)
+plot(fit[[4]],which=11)
+plot(fit[[5]],which=11)
 # eGARCH ====
 fit=garchfit.eGARCH;n.list = 1
 z = rugarch::residuals(fit[[n.list]],standardize=T)
 tmp.1 <- rugarch::GMMTest(z)
 tmp.1
-
 
 n.list = 2
 z = rugarch::residuals(fit[[n.list]],standardize=T)
@@ -50,3 +55,37 @@ n.list = 5
 z = rugarch::residuals(fit[[n.list]],standardize=T)
 tmp.5<- rugarch::GMMTest(z)
 tmp.5
+par(mfrow=c(5,1), mar = c(1,1,1,1))
+plot(fit[[1]],which=11)
+plot(fit[[2]],which=11)
+plot(fit[[3]],which=11)
+plot(fit[[4]],which=11)
+plot(fit[[5]],which=11)
+
+#NAGARCH ====
+fit=garchfit.fGARCH.NAGARCH
+par(mfrow=c(5,1), mar = c(1,1,1,1))
+plot(fit[[1]],which=11)
+plot(fit[[2]],which=11)
+plot(fit[[3]],which=11)
+plot(fit[[4]],which=11)
+plot(fit[[5]],which=11)
+
+# AVGARCH ====
+fit=garchfit.fGARCH.AVGARCH
+par(mfrow=c(5,1), mar = c(1,1,1,1))
+plot(fit[[1]],which=11)
+plot(fit[[2]],which=11)
+plot(fit[[3]],which=11)
+plot(fit[[4]],which=11)
+plot(fit[[5]],which=11)
+
+# TGARCH ====
+fit=garchfit.fGARCH.TGARCH
+par(mfrow=c(5,1), mar = c(1,1,1,1))
+plot(fit[[1]],which=11)
+plot(fit[[2]],which=11)
+plot(fit[[3]],which=11)
+plot(fit[[4]],which=11)
+plot(fit[[5]],which=11)
+
